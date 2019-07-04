b440417c901b22b633545946dd48a7e1a8221981
Reimer Behrends <behrends@gmail.com>
2019-07-05 01:13:16 +0200

    Gave Hecke.jl tests a longer timeout.

--- tests/config.py
+++ tests/config.py
@@ -2,7 +2,7 @@
     Test(name="GAP", script="test-gap.sh"),
     Test(name="GAP.jl", script="test-gapjl.sh"),
     Test(name="Nemo.jl", script="test-nemo.sh"),
-    Test(name="Hecke.jl", script="test-hecke.sh"),
+    Test(name="Hecke.jl", script="test-hecke.sh", timeout=3600),
     Test(name="AbstractAlgebra.jl", script = "test-absalg.sh"),
     Test(name="Singular.jl", script = "test-singularjl.sh"),
     Test(name="Polymake.jl", script = "test-polymakejl.sh"),
