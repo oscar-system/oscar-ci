e597cfec57a54ec515a3b3f66ba8cba7e0564b32
Florian Heiderich <florian@heiderich.org>
2019-07-08 10:23:35 +0200

    install Singular and test HomalgProject.jl

--- packages-bin.jl
+++ packages-bin.jl
@@ -5,6 +5,7 @@ Nemo = PackageSpec("Nemo")
 Hecke = PackageSpec("Hecke")
 Singular = PackageSpec("Singular")
 Polymake = PackageSpec("Polymake")
+HomalgProject = PackageSpec("HomalgProject") # Does not work yet
 
 Pkg.add(GAP)
 Pkg.add(AbstractAlgebra)
@@ -12,5 +13,6 @@ Pkg.add(Nemo)
 Pkg.add(Hecke)
 Pkg.add(Singular)
 Pkg.add(Polymake)
+Pkg.add(HomalgProject)
 
 Pkg.update()
