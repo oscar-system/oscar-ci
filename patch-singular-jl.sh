03f27149314503f063d71e3c9ed73657e2b40787
Reimer Behrends <behrends@gmail.com>
2020-04-26 22:59:32 +0200

    Remove unused files.

--- patch-singular-jl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SINGULAR_JL="$WORKSPACE/Singular.jl"
-git -C "$SINGULAR_JL" show HEAD:deps/build.jl | \
-awk '{ sub("https://github[.]com/Singular/Sources[.]git",
-  ENVIRON["WORKSPACE"] "/singular"); print $0; }' | \
-cat > $SINGULAR_JL/deps/build.jl
