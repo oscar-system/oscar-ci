288918b08e07694f96668a1bcde17246bf418ee8
Reimer Behrends <behrends@gmail.com>
2020-04-26 20:10:36 +0200

    Refine package installation process.

--- install/install-oscar.jl
+++ install/install-oscar.jl
@@ -5,10 +5,64 @@ packages = [
   "Singular", "Polymake", "HomalgProject", "Oscar"
 ]
 
+build_type = get(ENV, "BUILDTYPE", "master")
+
 Master(name) = PackageSpec(path=string(name, ".jl"))
 Stable(name) = PackageSpec(name=name)
-GetPackageSpec(name) = get(ENV, "BUILDTYPE", "master") == "master" ?
-  Master(name) : Stable(name)
+GetPackageSpec(name) = build_type == "master" ? Master(name) : Stable(name)
+
+function toposort(graph::Dict{T, Set{T}})::Vector{T} where T
+  graph = copy(graph)
+  vertices = keys(graph)
+  for (v, e) in graph
+    intersect!(graph[v], vertices)
+  end
+  out = Vector{T}()
+  while true
+    # find all with no successors
+    leaves = Vector{T}()
+    for (v, e) in graph
+      if isempty(e)
+        push!(leaves, v)
+      end
+    end
+    if isempty(leaves)
+      break
+    end
+    append!(out, leaves)
+    for v in leaves
+      delete!(graph, v)
+    end
+    for v in keys(graph)
+      setdiff!(graph[v], leaves)
+    end
+  end
+  # add any cycles
+  append!(out, keys(graph))
+  return out
+end
+
+function dep_graph(pkgs::Array{String,1})
+  graph = Dict{String, Set{String}}()
+  for pkg in pkgs
+    deps = Set()
+    try
+      proj = Pkg.TOML.parse(open(f->read(f, String),
+          string(pkg, ".jl/Project.toml")))
+      deps = Set(keys(proj["compat"]))
+    catch; end
+    graph[pkg] = deps
+  end
+  return graph
+end
+
+# Unless we're going for an end user build, we're adding the
+# packages in order of their dependencies to see if anything
+# breaks if we have the newest packages.
+
+if build_type != "user"
+  packages = toposort(dep_graph(packages))
+end
 
 pkglog = ".pkgerrors"
 
@@ -39,8 +93,10 @@ function Add(name)
   end
 end
 
-for pkg in packages
-  Add(pkg)
+if build_type == "user"
+  Add("Oscar")
+else
+  for pkg in packages
+    Add(pkg)
+  end
 end
-
-Pkg.update()
