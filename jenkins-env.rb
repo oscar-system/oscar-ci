fdef461ed93e798f61f37ed85ef0d550fdef6535
Reimer Behrends <behrends@gmail.com>
2020-09-10 15:12:27 +0200

    Allow local modifications of Jenkins environment.

--- jenkins-env.rb
+++ jenkins-env.rb
@@ -1,3 +1,15 @@
+require "yaml"
+
+begin
+  extra_env = YAML.safe_load(File.read(File.expand_path("~/.oscar-ci.yaml")))
+rescue
+  extra_env = {}
+end
+
+if extra_env.is_a?(Hash) and extra_env["env"].is_a?(Hash) then
+  ENV.update(extra_env["env"] || {})
+end
+
 $OSCAR_CI_NAME = ENV["OSCAR_CI_NAME"] || "oscar-ci"
 $OSCAR_CI_IMAGE = ENV["OSCAR_CI_IMAGE"] || $OSCAR_CI_NAME
 $JENKINS_HOME = ENV["JENKINS_HOME"] ||
