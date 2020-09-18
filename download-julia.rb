27d495195b7c107763bbc4701b81f29170c4b0b9
Reimer Behrends <behrends@gmail.com>
2020-09-18 12:46:42 +0200

    Enable downloading Julia for ARM machines.

--- download-julia.rb
+++ download-julia.rb
@@ -1,14 +1,17 @@
 #!/usr/bin/env ruby
 require_relative "settings.rb"
 require_relative "utils.rb"
+require "etc"
 
 version = ARGV.first
 major_version = version.split(".")[0..1].join(".")
-url = "https://julialang-s3.julialang.org/bin/linux/x64/#{major_version}/julia-#{version}-linux-x86_64.tar.gz"
+arch = Etc.uname[:machine]
+archdir = if arch == "x86_64" then "x64" else arch end
+url = "https://julialang-s3.julialang.org/bin/linux/#{archdir}/#{major_version}/julia-#{version}-linux-#{arch}.tar.gz"
 
 
 Dir.chdir($WORKSPACE) do
-  system! "wget", "-q", "-t", "5", "-N", "--no-if-modified-since", url
+  system! "wget", "-t", "5", "-N", "--no-if-modified-since", url
   FileUtils.rm_tree "julia"
   FileUtils.mkdir_p "julia"
   julia_tar_gz = File.basename(url)
